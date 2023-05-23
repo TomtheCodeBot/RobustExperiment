b781b7a8f79328a8175bed62224bf0c9ba1da46e
TomTheCodeBot <caoduyh@gmail.com>
2023-05-23 03:50:58 +0000

    update results

--- run_experiment_baidu.sh
+++ run_experiment_baidu.sh
@@ -1,8 +1,8 @@
-python for_baidu/baidu_agnews.py --parallel --model bert --defense mask
-python for_baidu/baidu_imdb.py --parallel --model bert --defense mask
-python for_baidu/baidu_agnews.py --parallel --model roberta --defense mask
-python for_baidu/baidu_imdb.py --parallel --model roberta --defense mask
 python for_baidu/baidu_agnews.py --parallel --model bert --defense safer
 python for_baidu/baidu_imdb.py --parallel --model bert --defense safer
 python for_baidu/baidu_agnews.py --parallel --model roberta --defense safer
 python for_baidu/baidu_imdb.py --parallel --model roberta --defense safer
+python for_baidu/baidu_agnews.py --parallel --model bert --defense mask
+python for_baidu/baidu_imdb.py --parallel --model bert --defense mask
+python for_baidu/baidu_agnews.py --parallel --model roberta --defense mask
+python for_baidu/baidu_imdb.py --parallel --model roberta --defense mask
